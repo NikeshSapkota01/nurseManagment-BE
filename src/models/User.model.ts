e79b30c8cee1ee8115e802ed6e7fd865facf7be0
Nikesh Sapkota <nikeshdev0@gmail.com>
2023-01-13 12:53:06 +0545

    feat: add model and types for user

---
+++ src/models/User.model.ts
@@ -0,0 +1,21 @@
+import db from "../db";
+import { IUser } from "../types/user.types";
+
+const TABLE_NAME = "User";
+
+export function fetchAllUsers() {
+  const result = db.select("id", "name", "email").from("User");
+  return result;
+}
+
+export const fetchUser = (id: Number) => {
+  return db(TABLE_NAME).select("id", "name", "email").where("id", id);
+};
+
+export const addNewUser = (data: IUser) => {
+  return db(TABLE_NAME).insert(data);
+};
+
+export const checkForUser = (name: String) => {
+  return db(TABLE_NAME).select().where("name", name);
+};
